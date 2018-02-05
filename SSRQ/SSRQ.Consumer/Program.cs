9aa6edd50848d38f675ed8af6f336bebab4e0d92
Zoran Gjuroski <zoran.gjuroski@gmail.com>
2018-02-05 02:47:52 +0100

    Initial commit

---
+++ SSRQ/SSRQ.Consumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SSRQ.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
